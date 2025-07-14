6c95b98797cedccdc94cc28204db92b6ffdb16a6
nwxxb <nawwabzia@gmail.com>
2025-07-14 12:28:04 +0700

    feat: init devise and homes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    confirmations: "users/confirmations",
+    omniauths: "users/omniauths",
+    passwords: "users/passwords",
+    registrations: "users/registrations",
+    sessions: "users/sessions",
+    unlocks: "users/unlocks"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "homes#landing_page"
 end
