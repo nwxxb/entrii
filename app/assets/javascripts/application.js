a524b15b4f265c85d7751f1655d52f2270d7665c
nwxxb <nawwabzia@gmail.com>
2025-07-20 17:29:05 +0700

    feat: add avatar uplaod field, add password required validation (frontend), and update auth spec

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,13 @@
 //= require jquery3
 //= require jquery_ujs
+//
+$(document).on('change', '.dropimage', function(e) {
+	var inputfile = this;
+	var reader = new FileReader();
+
+	reader.onloadend = function() {
+		$(inputfile).css('background-image', 'url(' + reader.result + ')');
+	}
+
+	reader.readAsDataURL(e.target.files[0]);
+});
