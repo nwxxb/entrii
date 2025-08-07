4a6bae6b212958c95dca5c1a9b9811de01ecd646
nwxxb <nawwabzia@gmail.com>
2025-08-07 17:43:07 +0700

    test: init submission spec failing test for create form

---
+++ spec/features/submissions_spec.rb
@@ -0,0 +1,89 @@
+require "rails_helper"
+
+RSpec.feature "Questions", :js do
+  it "user can submit a new submission on a questionnaire with question sorted by position and name (asc)" do
+    user = create(:user)
+    questionnaire = create(:questionnaire, user: user)
+
+    questions = [
+      create(:question, questionnaire: questionnaire, position: 0),
+      create(:question, :text, name: "name", questionnaire: questionnaire, position: 12),
+      create(:question, :number, name: "naps amount", questionnaire: questionnaire, position: 12),
+      create(:question, :number, name: "naps count", questionnaire: questionnaire, position: 12),
+      create(:question, :number, name: "rating", questionnaire: questionnaire, position: 999)
+    ]
+
+    submission_values = questions.map do |question|
+      build(:submission_value, question: question)
+    end
+
+    nonexisting_question = create(:question)
+
+    sign_in(user)
+
+    visit questionnaire_path(questionnaire)
+
+    first("button, a", text: "log new data").click
+
+    expect(page).to have_current_path(new_questionnaire_submissions_path(questionnaire))
+
+    expect(page).to have_content(questionnaire.title)
+    expect(page).to have_content(questionnaire.description)
+
+    within("form[action='#{questionnaire_submissions_path(questionnaire)}']") do
+      all(:fillable_field, name: /submission_value/).zip(submission_values).each do |field, submission_value|
+        field.fill_in with: submission_value.value
+      end
+
+      find("[type='submit']").click
+    end
+
+    expect(page).to have_current_path(questionnaire_path(questionnaire))
+    expect(page).to have_content(questionnaire.title)
+    expect(page).to have_table(with_rows: [
+      questions.zip(submission_values).to_h { |pair| [pair.first.name, pair.second.value] }
+    ])
+    expect(page).not_to have_content(nonexisting_question.name)
+  end
+
+  it "show validation error and prevent submit on submission form" do
+    user = create(:user)
+    questionnaire = create(:questionnaire, user: user)
+
+    questions = [
+      create(:question, :number, name: "a number field", questionnaire: questionnaire),
+      create(:question, :text, name: "non emptyable field", questionnaire: questionnaire),
+      create(:question, :text, name: "field that user answer correctly", questionnaire: questionnaire)
+    ]
+
+    submission_values = [
+      build(:submission_value, value: "asjdflaksjf", question: questions[0]),
+      build(:submission_value, value: "", question: questions[1]),
+      build(:submission_value, value: "a correct answer", question: questions[2])
+    ]
+
+    sign_in(user)
+
+    visit questionnaire_path(questionnaire)
+
+    first("button, a", text: "log new data").click
+
+    expect(page).to have_current_path(new_questionnaire_submissions_path(questionnaire))
+
+    expect(page).to have_content(questionnaire.title)
+    expect(page).to have_content(questionnaire.description)
+
+    within("form[action='#{questionnaire_submissions_path(questionnaire)}']") do
+      all(:fillable_field, name: /submission_value/).zip(submission_values).each do |field, submission_value|
+        field.fill_in with: submission_value.value
+      end
+
+      find("[type='submit']").click
+    end
+
+    expect(page).to have_current_path(new_questionnaire_submissions_path(questionnaire))
+    expect(page).to have_content(questionnaire.title)
+    expect(page).to have_content("can't be blank")
+    expect(page).to have_content("not a number")
+  end
+end
