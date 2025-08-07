8da5537b8df7cf3a2175cf51b23dde34bfda7c34
nwxxb <nawwabzia@gmail.com>
2025-08-07 17:50:06 +0700

    feat: init controller and views to create submission

---
+++ app/controllers/submissions_controller.rb
@@ -0,0 +1,27 @@
+class SubmissionsController < ApplicationController
+  def new
+    @questionnaire = current_user.questionnaires.find(params[:questionnaire_id])
+    @submission = @questionnaire.submissions.new
+    @questionnaire.questions.each do |question|
+      @submission.submission_values.new(question: question)
+    end
+  end
+
+  def create
+    @questionnaire = current_user.questionnaires.find(params[:questionnaire_id])
+    @submission = @questionnaire.submissions.new(submission_params)
+
+    if @submission.save
+      redirect_to questionnaire_path(@questionnaire)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def submission_params
+    params.require(:submission)
+      .permit(submission_values_attributes: [:question_id, :value])
+  end
+end
