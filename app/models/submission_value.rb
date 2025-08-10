e0db7bcbd74f09a9faa6cfa460788eca4aadf4b4
nwxxb <nawwabzia@gmail.com>
2025-08-10 16:53:58 +0700

    fix: add questionnaire relation to submission_value and it's default value callback

--- app/models/submission_value.rb
+++ app/models/submission_value.rb
@@ -1,9 +1,19 @@
 class SubmissionValue < ApplicationRecord
+  belongs_to :questionnaire
   belongs_to :submission
   belongs_to :question
 
   validates :value, presence: {unless: :is_emptyable?}
   validates :value, numericality: true, if: :number?
+
+  after_initialize :set_questionnaire_defaults
+
+  def set_questionnaire_defaults
+    if new_record?
+      derive_questionnaire_id = submission&.questionnaire_id || question&.questionnaire_id
+      self.questionnaire_id ||= derive_questionnaire_id
+    end
+  end
 
   def number?
     question.number?
