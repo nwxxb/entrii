9d2f4817188e5858261c38a13dcfbf7f63928ee3
nwxxb <nawwabzia@gmail.com>
2025-09-19 14:04:42 +0700

    fix: move stylesheet to webpacker and fetch open props from npm

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,14 @@
 module.exports = {
   plugins: [
-    require('postcss-import'),
-    require('postcss-flexbugs-fixes'),
-    require('postcss-preset-env')({
-      autoprefixer: {
-        flexbox: 'no-2009'
-      },
-      stage: 3
-    })
+    // postcss doesn't play nicely with open-props
+    // maybe we can re-enable it after upgrading rails and it's asset management
+    // require('postcss-import'),
+    // require('postcss-flexbugs-fixes'),
+    // require('postcss-preset-env')({
+    //   autoprefixer: {
+    //     flexbox: 'no-2009'
+    //   },
+    //   stage: 3
+    // }),
   ]
 }
