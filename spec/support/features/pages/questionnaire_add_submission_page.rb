288365cc68e89e8508828fca5d07fa58b61c7cf5
nwxxb <nawwabzia@gmail.com>
2025-10-08 11:59:47 +0700

    test: add prefill submission form spec and refactor some page object for testing

--- spec/support/features/pages/questionnaire_add_submission_page.rb
+++ spec/support/features/pages/questionnaire_add_submission_page.rb
@@ -1,12 +1,12 @@
 class QuestionnaireAddSubmissionPage < SitePrism::Page
-  set_url "/questionnaires{/id}/submissions/new"
+  set_url "/questionnaires{/id}/submissions/new{?query*}"
 
   section :hero, QuestionnaireHeroSection
   element :subnavbar, "#questionnaire-subnavbar"
   section :main_view, "#questionnaire-main-view" do
     sections :question_fields, "fieldset" do
-      element :question_label, "label"
-      element :question_field, "input"
+      element :label, "label"
+      element :input, "input"
     end
     element :submit_btn, "input[type='submit']"
   end
